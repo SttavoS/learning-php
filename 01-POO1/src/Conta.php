f7d72cd4ccb9b4985880ee312e9a6c7ba8de0ac2
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-02-21 21:24:16 -0300

    add methods to Conta.php

--- 01-POO1/src/Conta.php
+++ 01-POO1/src/Conta.php
@@ -5,4 +5,35 @@ class Conta
     public string $cpfTitular;
     public string $nomeTitular;
     public float $saldo;
+
+    public function sacar(float $valorASacar): void
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->saldo -= $valorASacar;
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+
+        $this->saldo += $valorADepositar;
+    }
+
+    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+    }
 }
